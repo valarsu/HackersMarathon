afd8236de43b20d8f4a941a4d78fc82db09cb6bd
haohongyang <haohongyang@vipkid.com.cn>
2019-10-09 15:19:43 +0800

    basic project

---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'A project that includes a video streaming and queuing system'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
