b7dcae1e500f99556255442f2d3b3809e536012c
haohongyang <haohongyang@vipkid.com.cn>
2019-10-10 12:00:00 +0800

    white bottom

--- src/fragment/mobileDemo.vue
+++ src/fragment/mobileDemo.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="demo-container">
     <div class="phone-container">
-
+      <div class="project-container">
+        手机内部显示<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>手机内部显示<br/>手机内部显示<br/>手机内部显示<br/>手机内部显示<br/>手机内部显示<br/>
+      </div>
     </div>
   </div>
 </template>
@@ -21,9 +23,9 @@ export default {
   position absolute
   z-index 1000
   top 0
-  left 70%
+  left 67%
   bottom 0px
-  width 30%
+  width 27%
   height 100vh
   padding 1vh 0
 .phone-container
@@ -32,4 +34,13 @@ export default {
   background-image url('../assets/phoneBackground.png')
   background-size 100% 98%
   background-repeat no-repeat
+.project-container
+  position relative
+  width 90%
+  height 77%
+  overflow auto
+  top 10%
+  left 5%
+  background #ffffff
+  border-radius 1%
 </style>
